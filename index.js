6da6bccf4696e1f4c2d6d0d6c87b8518d73195be
EC2 Default User <ec2-user@ip-172-31-7-78.us-east-2.compute.internal>
2018-02-10 21:30:34 +0000

    initial release

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const mongo = require('mongodb').MongoClient;
 const checkUrl = require('./checkUrl.js');
+const path = require('path');
 
+var ip = process.env.IP;
 const port = process.env.PORT || 8080;
 
 const app = express();
 
-const baseUrl = "http://localhost:8080/"
+const baseUrl = "http://"+ip+":"+port+"/"
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:shurl', (req,res) => {
+    mongo.connect('mongodb://'+ip+':27017', function(err,db) {
+        console.log(req.params.shurl);
+        if (err) throw err;
+        let dbo = db.db('shurl_db');
+        
+        dbo.collection('urls').findOne({short_url: +req.params.shurl}, function(err, doc){
+            if(err) throw err;
+            console.log(doc);
+           if(doc){
+               res.redirect(301, 'https://' + doc.original_url);
+           } else {
+            res.writeHead(400, {"Content-Type": "text/plain"});
+            res.end("This shortened url does not exist. Please use /create/<url> call to create a shortened url");
+           }
+           db.close();
+        });
+    });
+});
 
 app.get('/create/:url', (req, res) => {
 
@@ -15,58 +38,57 @@ app.get('/create/:url', (req, res) => {
 
         if (checkUrl(req.params.url)) { // check if valid url
 
-            mongo.connect('mongodb://localhost:27017', (err, db) => { //connect to mongo
+            mongo.connect('mongodb://'+ip+':27017', (err, db) => { //connect to mongo
                 if (err) throw err;
                 let dbo = db.db('shurl_db');
                 //find and return to var url in collection if it exists
-                let docExist = dbo.collection('urls').findOne({
-                    orig_url: req.params.url
-                }, (err, result) => {
+                let docExist;
+                dbo.collection('urls').findOne({
+                    original_url: req.params.url
+                }, function(err, result) {
                     if (err) throw err;
-                    return result;
-                });
-                console.log(docExist);
-                //check if url exists in collection
-                if (docExist == undefined) {
+                    docExist = result;
+                    console.log('findOne = '+ docExist);
+                                    //check if url exists in collection
+                if (docExist == null) {
+                    console.log('made it to if statement');
                     var newDoc;
+                    
                     dbo.collection('urls').find().sort({
                         'short_url': -1
                     }).limit(1).forEach(doc => {
                         //create newdoc ready for insertion
-
-                        if (doc == undefined) {
+                        console.log('forEach sort = '+ doc);
                             newDoc = {
-                                "orig_url": req.params.url,
-                                "short_url": 1
-                            };
-                        } else {
-                            newDoc = {
-                                "orig_url": req.params.url,
+                                "original_url": req.params.url,
                                 "short_url": doc.short_url + 1
                             };
-                        }
+                        
+                        
 
                         dbo.collection('urls').insertOne(newDoc, (err, result) => { // instert new url
                             if (err) throw err;
                             //console.log(result);
                             //console.log(newId);
-                            let shurl = {
+                            let resJSON = {
                                 original_url: req.params.url,
-                                short_url: baseUrl + result.ops[0].short_url
+                                short_url: baseUrl + newDoc.short_url
                             };
+                            console.log('doc inserted and about to close connection');
                             db.close();
                             res.writeHead(200, {
                                 "Content-Type": "text/plain"
                             });
-                            res.end(JSON.stringify(shurl));
+                            res.end(JSON.stringify(resJSON));
                         });
-
-                    });
+                });
+            
 
                 } else {
+                    console.log('didnt insert anything and about to close connection');
                     let resJSON = {
                         original_url: req.params.url,
-                        short_url: baseUrl + '/' + newId
+                        short_url: baseUrl + docExist.short_url
                     };
                     db.close();
                     res.writeHead(200, {
@@ -74,8 +96,9 @@ app.get('/create/:url', (req, res) => {
                     });
                     res.end(JSON.stringify(resJSON));
                 }
+                    });
 
-            });
+            }); //end of mongo client connect
 
         } else {
             res.writeHead(400, {
@@ -94,6 +117,12 @@ app.get('/create/:url', (req, res) => {
         });
         res.end("There was an error on the server");
     }
+});
+
+
+app.all('/*', (req,res) =>{
+   res.writeHead(404, {"Content-Type": "text/plain"});
+   res.end("Page not found. Please submit shortened url or use /create/<url> to create a shortened url");
 });
 
 app.listen(port);
