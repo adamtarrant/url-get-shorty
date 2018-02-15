6af822b06ed4b3529be9d23597e7a065d0b8cf1f
Adam <16904399+adamtarrant@users.noreply.github.com>
2018-02-15 12:39:17 +0000

    Update config_prod.js

--- config/config_prod.js
+++ config/config_prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     "mongoUri": process.env.MONGODB_URI,
-    "dbName": "heroku_k373dz4n",
-    "baseUrl": "https://url-get-shorty-refactored.herokuapp.com/",
+    "dbName": "heroku_dlwx7mjs",
+    "baseUrl": "https://url-get-shorty.herokuapp.com/",
     "port": process.env.PORT,
 }
